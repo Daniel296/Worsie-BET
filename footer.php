f26fbe8aafa8adf789c31cc870d98e4dae939e5d
Iordache Roland <roland.iordache96@gmail.com>
2017-04-24 01:28:24 +0300

    Add files via upload
    footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+<div id="footer">
+	<div class="bottom-menu">
+		<a href="index.php" class="active">Acasa</a> | 
+		<a href="pariuri.php">Pariuri</a> | 
+		<a href="rezultate.php">Rezultate</a> |
+		<a href="regulament.php">Regulament</a> | 
+		<a href="desprenoi.php">Despre Noi</a>
+	</div>
+	
+	<div class="pay-methods">
+		<a href="https://www.visa.ro/" target="blank"><img src="images/visa.png"></a>
+		<a href="http://www.maestrocard.com/gateway/index.html" target="blank"><img src="images/maestro.png"></a>
+		<a href="https://www.mastercard.ro/ro-ro.html" target="blank"><img src="images/mastercard.png"></a>
+		<a href="https://www.skrill.com" target="blank"><img src="images/skrill.png"></a>
+		<a href="https://www.paysafecard.com/ro-ro/" target="blank"><img src="images/paysafe.png"></a>
+	</div>
+
+	<div class="copyright">
+		Copyright &copy 2017 WorsieBet
+	</div>
+</div>
