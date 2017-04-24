b4973c91bed1225a4096169cb394daf62909e647
Daniel <daniel.oana26@gmail.com>
2017-04-24 23:35:34 +0300

    Add files via upload

--- rezultate.php
+++ rezultate.php
@@ -4,6 +4,7 @@
 	<title>Rezultate - WorsieBet</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/style-header.css">
+	<link rel="stylesheet" type="text/css" href="css/popup-style.css">
 </head>
 
 <body>
@@ -12,7 +13,7 @@
 	require('pages/header.php');
 ?>
 
-<div id="main">
+<div id="main-results">
 
 	<div class="search-bar">
 		<form action="" method="POST">
@@ -52,6 +53,188 @@
 	</div>
 	
 	
+	<div class ="bet-details">
+		<div class="results-bar">
+			<span>Ludlow</span>
+			<div class="results-times">
+				<div class="active-time">
+					<a href="">13:20</a>
+				</div>
+				<a href="">14:20</a>
+				<a href="">15:20</a>
+				<a href="">16:20</a>
+				<a href="">17:10</a>
+				<a href="">18:20</a>
+				<a href="">19:20</a>
+			</div>
+		</div>
+		<div class="results-head">
+			<div class="dim50">
+				<span>#</span>
+			</div>
+			<div class="dim200">
+				<span>Cal</span>
+			</div>
+			<div class="dim200">
+				<span>Jocheu / Antrenor</span>
+			</div>
+			<div class="dim50">
+				<span>Ani</span>
+			</div>
+			<div class="dim100">
+				<span>WR Cal</span>
+			</div>
+			<div class="dim100">
+				<span>WR Jocheu</span>
+			</div>
+		</div>
+		<div class="results-body">
+			<div class="results-team">
+				<div class="dim50">
+					<span>1.</span>
+				</div>
+				<div class="dim200">
+					<span>Burtonwood</span>
+				</div>
+				<div class="dim200">
+					<span>P Morris/Kieran Schofield</span>
+				</div>
+				<div class="dim50">
+					<span>6</span>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>30%</span>
+					</div>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>80%</span>
+					</div>
+				</div>
+			</div>
+			<div class="results-team">
+				<div class="dim50">
+					<span>2.</span>
+				</div>
+				<div class="dim200">
+					<span>Pushkin Museum</span>
+				</div>
+				<div class="dim200">
+					<span>D M Loughnane / David Egan</span>
+				</div>
+				<div class="dim50">
+					<span>4</span>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>65%</span>
+					</div>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>73%</span>
+					</div>
+				</div>
+			</div>
+			<div class="results-team">
+				<div class="dim50">
+					<span>3.</span>
+				</div>
+				<div class="dim200">
+					<span>Mr Chuckles</span>
+				</div>
+				<div class="dim200">
+					<span>D M Loughnane / David Egan</span>
+				</div>
+				<div class="dim50">
+					<span>5</span>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>86%</span>
+					</div>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>75%</span>
+					</div>
+				</div>
+			</div>
+			<div class="results-team">
+				<div class="dim50">
+					<span>4.</span>
+				</div>
+				<div class="dim200">
+					<span>Burtonwood</span>
+				</div>
+				<div class="dim200">
+					<span>P Morris/Kieran Schofield</span>
+				</div>
+				<div class="dim50">
+					<span>6</span>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>30%</span>
+					</div>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>80%</span>
+					</div>
+				</div>
+			</div>
+			<div class="results-team">
+				<div class="dim50">
+					<span>NR.</span>
+				</div>
+				<div class="dim200">
+					<span>Pushkin Museum</span>
+				</div>
+				<div class="dim200">
+					<span>D M Loughnane / David Egan</span>
+				</div>
+				<div class="dim50">
+					<span>4</span>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>65%</span>
+					</div>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>73%</span>
+					</div>
+				</div>
+			</div>
+			<div class="results-team">
+				<div class="dim50">
+					<span>NR.</span>
+				</div>
+				<div class="dim200">
+					<span>Mr Chuckles</span>
+				</div>
+				<div class="dim200">
+					<span>D M Loughnane / David Egan</span>
+				</div>
+				<div class="dim50">
+					<span>5</span>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>86%</span>
+					</div>
+				</div>
+				<div class="dim100">
+					<div class="win-chance">
+						<span>75%</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class ="bet-details">
 		<div class="results-bar">
 			<span>Ludlow</span>
