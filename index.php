d182744f19a9f206a84d3d3405cd7bc319a8daa8
Daniel <daniel.oana26@gmail.com>
2017-04-24 23:35:10 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Acasa - WorsieBet</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/style-header.css">
+	<link rel="stylesheet" type="text/css" href="css/popup-style.css">
 </head>
 <body>
 <?php
@@ -13,7 +14,7 @@
 <div id="main">
 	<div class="thumbnail" onMouseOut="hide('bet-now')" onMouseOver="show('bet-now')" >
 		<img src="images/betting-img.jpg" id="betting-img">
-		<a href="pages/Pariuri.php?date=<?php echo date("Y-m-d", time());?>">
+		<a href="pariuri.php?date=<?php echo date("Y-m-d", time());?>">
 			<div id="bet-now">
 				<span id="bet-now-text">PARIAZA ACUM</span>
 			</div>
