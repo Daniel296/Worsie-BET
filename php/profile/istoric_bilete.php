64ae530a4dfca645efbee0031f0f9137c56034c0
tpetru <tpetru@users.noreply.github.com>
2017-04-25 00:19:20 +0300

    Add files via upload

--- php/profile/istoric_bilete.php
+++ php/profile/istoric_bilete.php
@@ -1,154 +1,192 @@
-<html>
+<!DOCTYPE html>
 <div id="bilete">
 	<!-- ================================================== -->
 	<div class="biletX">
-		<div class="biletID">ID: 17apr1812009963512873612983123120
+		<div class="biletID">ID: 17apr1812009963
 		</div>
 
 		<div class="biletData">18.04.2017
 		</div>
 
-		<div class="biletCota">Cota: 3.50
+		<div class="biletSuma">Suma: 10 RON
 		</div>
 
-		<div class="biletTipX">X
+		<div class="biletCastig">Castig: 0 RON
+		</div>
+
+		<div class="biletCota">Cota: 3.50
 		</div>
 
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletO">
-		<div class="biletID">ID: 17apr1710546548654685421564646513
+		<div class="biletID">ID: 17apr1712009963
 		</div>
 
 		<div class="biletData">17.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 12.40 RON
+		</div>
+		
 		<div class="biletCota">Cota: 1.24
 		</div>
 
-		<div class="biletTipO">O
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletO">
-		<div class="biletID">ID: 17apr1619654186534686543156446464
+		<div class="biletID">ID: 17apr1612009963
 		</div>
 
 		<div class="biletData">16.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 68.40 RON
+		</div>
+		
 		<div class="biletCota">Cota: 6.84
 		</div>
 
-		<div class="biletTipO">O
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletO">
-		<div class="biletID">ID: 17apr1578965136461235646212311845
+		<div class="biletID">ID: 17apr1512009963
 		</div>
 
 		<div class="biletData">15.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 34.00 RON
+		</div>
+		
 		<div class="biletCota">Cota: 3.40
 		</div>
 
-		<div class="biletTipO">O
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletX">
-		<div class="biletID">ID: 17apr1478965136461235646212311845
+		<div class="biletID">ID: 17apr1412009963
 		</div>
 
 		<div class="biletData">14.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 0 RON
+		</div>
+		
 		<div class="biletCota">Cota: 2.00
 		</div>
 
-		<div class="biletTipX">X
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletX">
-		<div class="biletID">ID: 17apr1378965136461235646212311845
+		<div class="biletID">ID: 17apr1312009963
 		</div>
 
 		<div class="biletData">13.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 0 RON
+		</div>
+		
 		<div class="biletCota">Cota: 2.00
 		</div>
 
-		<div class="biletTipX">X
-		</div>
 	</div>
 	
 	<!-- ================================================== -->
 	<div class="biletO">
-		<div class="biletID">ID: 17apr1378965136461235646212311845
+		<div class="biletID">ID: 17apr1212009963
 		</div>
 
 		<div class="biletData">13.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 73.50 RON
+		</div>
+		
 		<div class="biletCota">Cota: 7.35
 		</div>
 
-		<div class="biletTipO">O
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletX">
-		<div class="biletID">ID: 17apr1278965136461235646212311845
+		<div class="biletID">ID: 17apr1212009963
 		</div>
 
 		<div class="biletData">12.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 0 RON
+		</div>
+		
 		<div class="biletCota">Cota: 2.85
 		</div>
 
-		<div class="biletTipX">X
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletO">
-		<div class="biletID">ID: 17apr1078965136461235646212311845
+		<div class="biletID">ID: 17apr1812009963
 		</div>
 
 		<div class="biletData">10.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 21.00 RON
+		</div>
+		
 		<div class="biletCota">Cota: 2.10
 		</div>
 
-		<div class="biletTipO">O
-		</div>
 	</div>
 
 	<!-- ================================================== -->
 	<div class="biletX">
-		<div class="biletID">ID: 17apr0978965136461235646212311845
+		<div class="biletID">ID: 17apr0912009963
 		</div>
 
 		<div class="biletData">09.04.2017
 		</div>
 
+		<div class="biletSuma">Suma: 10 RON
+		</div>
+
+		<div class="biletCastig">Castig: 42.50 RON
+		</div>
+		
 		<div class="biletCota">Cota: 4.25
 		</div>
 
-		<div class="biletTipX">X
-		</div>
 	</div>
 </div>
-</html>
