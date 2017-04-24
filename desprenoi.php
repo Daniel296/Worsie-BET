a52e311399afb583a7d5d2930936ef2e49cc4611
tpetru <tpetru@users.noreply.github.com>
2017-04-24 23:19:51 +0300

    Add files via upload

---
+++ desprenoi.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Despre noi - WorsieBet</title>
+	<link rel="stylesheet" type="text/css" href="css/about-style.css">
+	<link rel="stylesheet" type="text/css" href="css/style-header.css">
+
+</head>
+<body>
+<?php
+	require('pages/header.php');
+?>
+
+<div id="about">
+	<h2>Despre</h2>
+	<p>Compania: WorsieBet SC</p>
+	<p>Locul și adresa:	Iași, România</p>
+	<p>Număr de identificare: RO01366904</p>
+	<p>TVA:	HR92265244213</p>
+	<p>Număr de înregistrare (CUI):	RO0802244,  Oficiul National al Registrului Comertului, România </p>
+	<p>Autorizație pentru organizarea de pariuri online în România:</p>
+	<p>Clasa: UP/I-461-05/17-01/25</p>
+	<p>NR. ÎNREGISTRARE: 413-07-21-07/17-2, Iași, 25 Aprilie 2017</p>
+	<p>Licență pentru organizarea jocurilor de noroc la distanță:</p>
+	<p>Licența nr. L1160652W000306, valabilă din data de 01.08.2016 până la 31.07.2026.</p>
+	<p>Website-ul ONJN:	onjn.gov.ro</p>
+
+</div>
+
+<?php
+	require('pages/footer.php');
+?>
+</body>
