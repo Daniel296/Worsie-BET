7466c53f171937c5141a973316f211aef9e85057
Tanasa Petru <tanasapetrut@hotmail.com>
2017-05-17 17:21:38 +0300

    Revert "DataBase_connection_bugs_fixed"
    This reverts commit 06f5fda5b68b31cf120959305c8cf86f0de62fa2.

--- php/database/db_connection.php
+++ php/database/db_connection.php
@@ -2,17 +2,11 @@
 class DbConnection {
     private static $instance;
     private $dbConn;
-	
-    private static $DB_HOST = "fenrir.info.uaic.ro";
+	private static $DB_HOST = "fenrir.info.uaic.ro";
 	private static $DB_USER = "WorsieBET";
 	private static $DB_PASS = "ykoFsp2TCB";
-	private static $DB_DB = "WorsieBET";
-
-	//private static $DB_HOST = "localhost";
-    // private static $DB_USER = "root";
-    // private static $DB_PASS = "";
-    // private static $DB_DB = "worsiebet";
-
+	
+	
     private function __construct() {}
 
     private static function getInstance(){
@@ -25,7 +19,7 @@ class DbConnection {
 
     private static function initConnection(){
         $db = self::getInstance();
-		$db->DbConnection =  new mysqli(self::$DB_HOST, self::$DB_USER, self::$DB_PASS, self::$DB_DB);
+		$db->DbConnection =  new mysqli(self::$DB_HOST, self::$DB_USER, self::$DB_PASS);
 		$db->DbConnection->set_charset('utf8');
 		return $db;
     }
