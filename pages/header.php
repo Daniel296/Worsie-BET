6faec476f48287a336090a631985de4716679e45
Daniel <daniel.oana26@gmail.com>
2017-05-21 21:39:50 +0300

    Encrypt password added

--- pages/header.php
+++ pages/header.php
@@ -64,7 +64,7 @@
 		<div class="container">
 			<div class="form-login">
 				<label><b>Username</b></label>
-				<input type="text" placeholder="Enter Username" name="username" required>
+				<input type="text" placeholder="Enter Username" name="username_login" required>
 			</div>
 
 			<div  class="form-login">
@@ -77,24 +77,25 @@
 	</form>
 </div>
 	<?php
+		/*===========LOGIN===========*/
 		/*Daca sunt setate datele necesare pentru logare, verificam daca exista contul*/
-		if (isset($_POST['username']) && isset($_POST['password'])) {
+		if (isset($_POST['username_login']) && isset($_POST['password'])) {
 
 			/* Scoatem caracterele speciale din username si parola pentru evitarea sql injection*/
-			$username = mysql_real_escape_string($_POST['username']);
+			$username = mysql_real_escape_string($_POST['username_login']);
 			$password = mysql_real_escape_string($_POST['password']);
 
 			/* Creeam si executam query-ul pentru a vedea daca exista user-ul in baza de date */
 			$stmt =  $conn->stmt_init();
-			$sql_query = "SELECT id, conectat FROM UTILIZATORI WHERE username = ? AND parola = ?";
+			$sql_query = "SELECT id, conectat, password FROM UTILIZATORI WHERE username = ?";
 
 			if($stmt =  $conn->prepare($sql_query)) {
 
-				$stmt->bind_param('ss', $username, $password);
+				$stmt->bind_param('s', $username);
 				$stmt->execute();
-				$stmt->bind_result($id_user, $conectat);
+				$stmt->bind_result($id_user, $conectat, $hash_password);
 				$stmt->fetch();
-				if(isset($id_user) and $conectat == 0) {
+				if(isset($id_user) and $conectat == 0 and password_verify($password, $hash_password) == TRUE) {
 				/* Pornim sesiunea si setam parametrii la sesiune */
 					$_SESSION['id'] = $id_user;
 					$_SESSION['username'] = $username;
@@ -144,6 +145,7 @@ window.onclick = function(event) {
 		</div>
 
 		<?php
+											/*=============REGISTER===========*/
 			/* Aceasta functie afiseaza mesajele de eroare care apar in urma validarii datelor introduse de utilizator*/
 			function print_error($err) {
 				echo "<p> *$err </p>";
@@ -158,13 +160,10 @@ window.onclick = function(event) {
 				$lastname = mysql_real_escape_string($_POST['lastname']);
 				$firstname = mysql_real_escape_string($_POST['firstname']);
 				$email = mysql_real_escape_string($_POST['email']);
-				// $day = mysql_real_escape_string($_POST['day']);
-				// $month = mysql_real_escape_string($_POST['month']);
-				// $year = mysql_real_escape_string($_POST['year']);
 				$password = mysql_real_escape_string($_POST['password']);
 				$re_password = mysql_real_escape_string($_POST['re_password']);
 				$birth_date = new DateTime($_POST['bday']);
-				//$birth_date =  new DateTime($_POST['day'] . "." . $_POST['month'] . "." . $_POST['year']);
+
 
 				/* Validam datele introduse de utilizator si afisam mesajele de eroare corespunzatoare */
 				if(strlen($username) < 6 or strlen($username) > 30)
@@ -183,6 +182,10 @@ window.onclick = function(event) {
 					print_error("Parolele sunt diferite");
 
 
+				/* Criptam parola */
+				$password = password_hash($password, PASSWORD_BCRYPT);
+
+
 				/* Verificam daca username-ul sau email-ul exista deja in baza de date */
 				$stmt =  $conn->stmt_init();
 				$sql_query = "SELECT username, email FROM utilizatori WHERE username = ? or email = ?";
@@ -191,24 +194,31 @@ window.onclick = function(event) {
 					$stmt->execute();
 					$stmt->bind_result($username1, $email1);
 					$stmt->fetch();
+					echo "H1 ";
 					if(isset($username1) == false and isset($email1) == false) {
 						unset($stmt);
 						$stmt =  $conn->stmt_init();
-						$date = date('d.m.y H:m:s');
-						echo "date: $date";
-						$sql_query = "INSERT INTO UTILIZATORI VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+						echo "H2 ";
+						$sql_query = "INSERT INTO UTILIZATORI VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 						if($stmt =  $conn->prepare($sql_query)) {
-							$stmt->bind_param('sssdssssssi', $username, $password, $email, 0, $lastname, $firstname, '', '', '', '', 0, $date);
+							$stmt->bind_param('dsssdsssisssdi', 0, $username, $password, $email, 0, $lastname, $firstname, '', $birth_date, '', '', '', 0, '');
 							$stmt->execute();
+							echo "H3 ";
 						}
 						else {
 							if(isset($username1) == true and $username1 == $username)
 								print_error("Acest username exista deja");
-								if(isset($email1) == true and $email1 == $email)
+							if(isset($email1) == true and $email1 == $email)
 								print_error("Aceasta adresa de email exista deja");
 							}
-
+							echo "H4 ";
 						}
+						else {
+							echo "Eroare 1";
+						}
+					}
+					else {
+						echo "Eroare 2";
 					}
 				}
 
@@ -234,119 +244,6 @@ window.onclick = function(event) {
 				<label><b>Data nasterii</b></label>
 				<input type="date" name="bday" required>
 			</div>
-			<!--
-			<div class="form-login">
-				<label><b>Data nasterii</b></label>
-				<div class="select-db">
-					<select name="day" required>
-						<option> Day </option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-						<option value="14">14</option>
-						<option value="15">15</option>
-						<option value="16">16</option>
-						<option value="17">17</option>
-						<option value="18">18</option>
-						<option value="19">19</option>
-						<option value="20">20</option>
-						<option value="21">21</option>
-						<option value="22">22</option>
-						<option value="23">23</option>
-						<option value="24">24</option>
-						<option value="25">25</option>
-						<option value="26">26</option>
-						<option value="27">27</option>
-						<option value="28">28</option>
-						<option value="29">29</option>
-						<option value="30">30</option>
-						<option value="31">31</option>
-					</select>
-
-					<select name="month" required>
-						<option> Month </option>
-						<option value="January">January</option>
-						<option value="Febuary">Febuary</option>
-						<option value="March">March</option>
-						<option value="April">April</option>
-						<option value="May">May</option>
-						<option value="June">June</option>
-						<option value="July">July</option>
-						<option value="August">August</option>
-						<option value="September">September</option>
-						<option value="October">October</option>
-						<option value="November">November</option>
-						<option value="December">December</option>
-					</select>
-
-					<select name="year" required>
-						<option> Year </option>
-						<option value="1999">1999</option>
-						<option value="1998">1998</option>
-						<option value="1997">1997</option>
-						<option value="1996">1996</option>
-						<option value="1995">1995</option>
-						<option value="1994">1994</option>
-						<option value="1993">1993</option>
-						<option value="1992">1992</option>
-						<option value="1991">1991</option>
-						<option value="1990">1990</option>
-						<option value="1989">1989</option>
-						<option value="1988">1988</option>
-						<option value="1987">1987</option>
-						<option value="1986">1986</option>
-						<option value="1985">1985</option>
-						<option value="1984">1984</option>
-						<option value="1983">1983</option>
-						<option value="1982">1982</option>
-						<option value="1981">1981</option>
-						<option value="1980">1980</option>
-						<option value="1979">1979</option>
-						<option value="1978">1978</option>
-						<option value="1977">1977</option>
-						<option value="1976">1976</option>
-						<option value="1975">1975</option>
-						<option value="1974">1974</option>
-						<option value="1973">1973</option>
-						<option value="1972">1972</option>
-						<option value="1971">1971</option>
-						<option value="1970">1970</option>
-						<option value="1969">1969</option>
-						<option value="1968">1968</option>
-						<option value="1967">1967</option>
-						<option value="1966">1966</option>
-						<option value="1965">1965</option>
-						<option value="1964">1964</option>
-						<option value="1963">1963</option>
-						<option value="1962">1962</option>
-						<option value="1961">1961</option>
-						<option value="1960">1960</option>
-						<option value="1959">1959</option>
-						<option value="1958">1958</option>
-						<option value="1957">1957</option>
-						<option value="1956">1956</option>
-						<option value="1955">1955</option>
-						<option value="1954">1954</option>
-						<option value="1953">1953</option>
-						<option value="1952">1952</option>
-						<option value="1951">1951</option>
-						<option value="1950">1950</option>
-						<option value="1949">1949</option>
-						<option value="1948">1948</option>
-						<option value="1947">1947</option>
-					</select>
-				</div>
-			</div>-->
 
 			<div class="form-login">
 				<label><b>Email</b></label>
