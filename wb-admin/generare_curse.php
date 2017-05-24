f9281c2d0220273adde21ee22f076339db4b4d41
Iordache Roland <roland.iordache96@gmail.com>
2017-05-25 02:58:32 +0300

    Some fixes
    #generare_curse fix insert into

--- wb-admin/generare_curse.php
+++ wb-admin/generare_curse.php
@@ -42,14 +42,23 @@
 					$cursa = $nume_curse[$index_cursa];
 
 					//cote_cai+jochei (5)
-					$cote=array(4.50,6.00,7.00,4.50,5.50,5.00,7.00,8.00,8.50,4.00,7.50,6.50);
+					$cote=array('4.50','6.00','7.00','4.50','5.50','5.00','7.00','8.00','8.50','4.00','7.50','6.50');
 					$index_cota = array_rand($cote,5);
-					$cota =$cote[$index_cota[0]]; //de 1,2,3,4...
+					$cota =$cote[$index_cota[0]] . ' ' . $cote[$index_cota[1]] . ' ' . $cote[$index_cota[2]] . ' ' . $cote[$index_cota[3]] . ' ' . $cote[$index_cota[4]];
+					
+					//sanse castig
+					$sanse=array('40','50','60','30','70','50');
+					$index_sanse = array_rand($sanse,5);
+					$sansa =$sanse[$index_sanse[0]] . ' ' . $sanse[$index_sanse[1]] . ' ' . $sanse[$index_sanse[2]] . ' ' . $sanse[$index_sanse[3]] . ' ' . $sanse[$index_sanse[4]];
 					
 					//vremea
-					$lista_vreme=array("10°C insorit", "14°C innorat", "19°C insorit", "17°C averse de ploaie", "21°C posibili stropi");
+					$lista_grade=array("10 ", "14", "19", "17", "21");
+					$index_grade=array_rand($lista_grade,1);
+					$grade=$lista_grade[$index_grade];
+					$lista_vreme = array("insorit","innorat","insorit","averse de ploaie", "posibili stropi");
 					$index_vreme=array_rand($lista_vreme,1);
 					$vreme=$lista_vreme[$index_vreme];
+					$insert_vreme = $grade . chr(176) . 'C - ' . $vreme;
 					
 					//cai
 					$connection = mysqli_connect('localhost', 'root', '', 'worsiebet');
@@ -61,7 +70,7 @@
 					while ($row = $res->fetch_assoc()) {
 						$cai = $cai.$row['id'].' ';
 					}
-					
+
 					//jochei
 					$res = mysqli_query($connection,"SELECT id FROM jochei ORDER BY rand() LIMIT 5");
 					if($res === FALSE) { 
@@ -72,7 +81,7 @@
 						$jochei = $jochei.$row['id'].' ';
 					}
 					
-					$insert = mysqli_query($connection,"INSERT INTO curse (nume, id_cai, id_jochei, vreme, data, sanse_castig, cote) VALUES ('$cursa','$cai','$jochei','$vreme',CAST('". $date1 ."' AS DATE),'0','$cota')");
+					$insert = mysqli_query($connection,"INSERT INTO curse (nume, id_cai, id_jochei, vreme, data, sanse_castig, cote) VALUES ('$cursa','$cai','$jochei','$insert_vreme',CAST('". $date1 ."' AS DATE),'$sansa','$cota')");
 					if($insert === FALSE) { 
 						die(mysql_error()); // TODO: better error handling
 					}
