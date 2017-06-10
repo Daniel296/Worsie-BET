93681dc47d51a898d1966b1537e451127cf0c108
Iordache Roland <roland.iordache96@gmail.com>
2017-06-10 05:34:58 +0300

    Fixat balanta cu minus
    Nu se pot plasa bilete daca nu sunt destui bani

--- php/insert-ticket.php
+++ php/insert-ticket.php
@@ -1,22 +1,33 @@
 <?php
     require("database/connect2DB.php");
 
-	/* Inseram biletul in baza de date */
-	unset($stmt);
-	$stmt =  $conn->stmt_init();
-	$sql_query = $_REQUEST['insert_ticket'];
-	if($stmt =  $conn->prepare($sql_query))
-		$stmt->execute();
-
 	$id_user = $_REQUEST['id'];
 	$bet_count = $_REQUEST['bet_count'];
+	
+	$res = mysqli_query($conn,"SELECT balanta FROM utilizatori WHERE id = '$id_user'");
+	if($res === FALSE) { 
+		die(mysql_error()); // TODO: better error handling
+	}
+	while ($row = $res->fetch_assoc()) {
+		$balanta = $row['balanta'];
+	}
+	if($balanta-$bet_count>0) {
+		/* Inseram biletul in baza de date */
+		unset($stmt);
+		$stmt =  $conn->stmt_init();
+		$sql_query = $_REQUEST['insert_ticket'];
+		if($stmt =  $conn->prepare($sql_query))
+			$stmt->execute();
 
-    /* Facem update la balanta si biletele in asteptare */
-	unset($stmt);
-	$stmt =  $conn->stmt_init();
-	$sql_query = "UPDATE utilizatori SET bilete_asteptare = bilete_asteptare + 1, bilete_total = bilete_total + 1, balanta = balanta - ? WHERE id = ?";
-	if($stmt =  $conn->prepare($sql_query)) {
-		$stmt->bind_param('ds', $bet_count, $id_user);
-		$stmt->execute();
+
+		/* Facem update la balanta si biletele in asteptare */
+		unset($stmt);
+		$stmt =  $conn->stmt_init();
+		
+		$sql_query = "UPDATE utilizatori SET bilete_asteptare = bilete_asteptare + 1, bilete_total = bilete_total + 1, balanta = balanta - ? WHERE id = ?";
+		if($stmt =  $conn->prepare($sql_query)) {
+			$stmt->bind_param('ds', $bet_count, $id_user);
+			$stmt->execute();
+		}
 	}
  ?>
